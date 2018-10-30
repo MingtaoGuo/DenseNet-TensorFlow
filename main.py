f4e7200ee07a2391252e1a81351e077dd02db571
MarTinGuo <gmt798714378@gmail.com>
2018-10-30 21:00:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--learning_rate", type=float, default=0.1)
     parser.add_argument("--weight_decay", type=float, default=1e-4)
-    parser.add_argument("--cifar10_path", type=str, default="E://数据集//cifar-10-batches-mat//")
+    parser.add_argument("--cifar10_path", type=str, default="./cifar10//")
     parser.add_argument("--class_nums", type=int, default=10)
     parser.add_argument("--epoch", type=int, default=100)
     parser.add_argument("--is_trained", type=int, default=True)
