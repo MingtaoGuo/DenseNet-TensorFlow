764570a7c9979780c9139de86d9ded757f1c9e4c
MarTinGuo <gmt798714378@gmail.com>
2018-10-30 22:43:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     parser.add_argument("--weight_decay", type=float, default=1e-4)
     parser.add_argument("--cifar10_path", type=str, default="./cifar10//")
     parser.add_argument("--class_nums", type=int, default=10)
-    parser.add_argument("--epoch", type=int, default=100)
+    parser.add_argument("--epoch", type=int, default=300)
     parser.add_argument("--is_trained", type=int, default=True)
 
     args = parser.parse_args()
